77041ded7e9781f16b43b11f40ab68624f3ef606
ProductiveRage <742348+ProductiveRage@users.noreply.github.com>
2016-07-07 20:20:19 +0100

    Add a URL rewriter for generating a GitHub Pages site

---
+++ NeoCitiesTransformer/SiteContentTransformer/ContentRewriting/DefaultGitHubPagesUrlRewriter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace NeoCitiesTransformer.SiteContentTransformer.ContentRewriting
+{
+	public static class DefaultGitHubPagesUrlRewriter
+	{
+		public static UrlRewriter Get(params Tuple<Uri, Uri>[] customOverrides)
+		{
+			return url =>
+			{
+				if (url == null)
+					throw new ArgumentNullException("url");
+
+				if (customOverrides != null)
+				{
+					var customOverride = customOverrides.FirstOrDefault(c => c.Item1.Equals(url));
+					if (customOverride != null)
+						return customOverride.Item2;
+				}
+
+				var urlString = url.ToString();
+				if (urlString.Contains('?'))
+					return null;
+
+				if (urlString.Contains("://"))
+					return url;
+				else if (urlString.StartsWith("//"))
+					return new Uri("https:" + urlString);
+
+				urlString = urlString.Replace('\\', '/');
+				if (urlString == "")
+					urlString = "index.html";
+
+				return new Uri(urlString, UriKind.RelativeOrAbsolute);
+			};
+		}
+	}
+}
